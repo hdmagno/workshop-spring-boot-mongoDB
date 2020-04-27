fe95a52aa2f7ce9106a426fc46daaf6f34ef820a
Hilton Magno <hdmagno@gmail.com>
2020-04-27 15:40:00 -0300

    Obtendo usuario por id

--- src/main/java/com/hdmagno/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/hdmagno/workshopmongo/repository/UserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.hdmagno.workshopmongo.domain.User;
 
+
 @Repository
-public interface UserRepository extends MongoRepository<User, String>  {
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
 
 }
--- src/main/java/com/hdmagno/workshopmongo/services/UserService.java
+++ src/main/java/com/hdmagno/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.hdmagno.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hdmagno.workshopmongo.domain.User;
 import com.hdmagno.workshopmongo.repository.UserRepository;
+import com.hdmagno.workshopmongo.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -14,8 +16,14 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
+	
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+	}
+	
 }
