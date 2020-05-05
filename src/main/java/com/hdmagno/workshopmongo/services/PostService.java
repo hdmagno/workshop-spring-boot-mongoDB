9ff1d346c1e29b317588ae68e9e6ca4763a2cecb
Hilton Magno <hdmagno@gmail.com>
2020-05-05 18:24:42 -0300

    Obtendo post por id

---
+++ src/main/java/com/hdmagno/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.hdmagno.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hdmagno.workshopmongo.domain.Post;
+import com.hdmagno.workshopmongo.repository.PostRepository;
+import com.hdmagno.workshopmongo.services.exception.ResourceNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+	}
+
+}
